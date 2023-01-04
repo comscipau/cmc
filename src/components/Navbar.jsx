5e3af0fd15597ca4216d6f255592575685de5e0c
Paulo Buenavente <46041015+e-t-n-e-v@users.noreply.github.com>
2023-01-05 01:36:49 +0800

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,39 @@
 import React from "react";
-import LOGO from "../assets/cmc-logo-outline.png";
+// import LOGO from "../assets/cmc-logo-outline.png";
 
 const Navbar = () => {
   return (
     <div className="w-full flex justify-between pt-[3.875rem] px-[8.81rem]">
-      <div>
-        <img src={LOGO} alt="LOGO" />
-      </div>
       <div className="flex">
-        <ul className="flex gap-[3.63rem] items-center text-justWhite">
-          <li className="hover:cursor-pointer">HOME</li>
-          <li className="hover:cursor-pointer">MEMBERSHIP</li>
-          <li className="hover:cursor-pointer">CLUB MINT</li>
-          <li className="hover:cursor-pointer">SHARKS MINT</li>
-          <li className="hover:cursor-pointer">FIAT VISA</li>
-          <li className="hover:cursor-pointer">CONTACT US</li>
-          <li className="hover:cursor-pointer">DEPOSIT FUNDS</li>
+        <ul className="flex gap-8 items-center text-justWhite font-inter text-base font-normal">
+          <li className="hover:cursor-pointer hover:text-gold hover:border-b hover:border-gold">
+            HOME
+          </li>
+          <li className="hover:cursor-pointer hover:text-gold hover:border-b hover:border-gold">
+            MEMBERSHIP
+          </li>
+          <li className="hover:cursor-pointer hover:text-gold hover:border-b hover:border-gold">
+            CLUB MINT
+          </li>
+          <li className="hover:cursor-pointer hover:text-gold hover:border-b hover:border-gold">
+            SHARKS MINT
+          </li>
+          <li className="hover:cursor-pointer hover:text-gold hover:border-b hover:border-gold">
+            FIAT VISA
+          </li>
+          <li className="hover:cursor-pointer hover:text-gold hover:border-b hover:border-gold">
+            CONTACT US
+          </li>
+          {/* <li className="hover:cursor-pointer hover:text-gold hover:border-b hover:border-gold">
+            DEPOSIT FUNDS
+          </li> */}
         </ul>
+      </div>
+      <div>
+        {/* <img src={LOGO} alt="LOGO" /> */}
+        <button className="text-white font-spacegrotesk font-bold text-base bg-gold py-[13px] px-[25px] rounded-[20px] border-[1px] border-gold radial-btn">
+          Join Our Community
+        </button>
       </div>
     </div>
   );
