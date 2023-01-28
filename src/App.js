09359451b0f5383e32768d5f8fe78c0fbe91b9aa
Paulo Buenavente <46041015+e-t-n-e-v@users.noreply.github.com>
2023-01-28 19:13:17 +0800

    overflow hidden

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="bg-justBlack">
+    <div className="bg-justBlack overflow-x-hidden">
       <Navbar />
       <Hero />
       <Membership />
